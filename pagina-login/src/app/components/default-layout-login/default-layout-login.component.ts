ea692150589f63e7edab887003e31bfc1e19d860
André Souza <andresouza_ti@yahoo.com>
2024-04-24 12:59:52 -0300

    feat: add pages, default e styles

---
+++ pagina-login/src/app/components/default-layout-login/default-layout-login.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-layout-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-layout-login.component.html',
+  styleUrl: './default-layout-login.component.scss'
+})
+export class DefaultLayoutLoginComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secundaryBtnText: string = "";
+}
